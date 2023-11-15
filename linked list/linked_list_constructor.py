ea8f512b9a2b614da43dc37ce709399cebc256e0
Jerricke <eejerrickw@gmail.com>
2023-11-14 23:59:15 -0800

    LL pop

--- linked list/linked_list_constructor.py
+++ linked list/linked_list_constructor.py
@@ -10,6 +10,45 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-my_linked_list = LinkedList(4)
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
 
-print(my_linked_list.head.value)
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else: 
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+my_linked_list = LinkedList(1)
+
+my_linked_list.append(2)
+
+
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+
+my_linked_list.print_list()
