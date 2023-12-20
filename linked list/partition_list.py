68e26c0d8600318240aeab88788eeb93882f04a2
Jerricke <eejerrickw@gmail.com>
2023-12-20 15:38:35 -0800

    partition list LL;

---
+++ linked list/partition_list.py
@@ -0,0 +1,90 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+        else:
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+            current_node.next = new_node
+        self.length += 1 
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next    
+            
+    def make_empty(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def partition_list(self, x):
+        if not self.head:
+            return None
+    
+        dummy1 = Node(0)
+        dummy2 = Node(0)
+        prev1 = dummy1
+        prev2 = dummy2
+        current = self.head
+    
+        while current:
+            if current.value < x:
+                prev1.next = current
+                prev1 = current
+            else:
+                prev2.next = current
+                prev2 = current
+            current = current.next
+    
+        prev1.next = None
+        prev2.next = None
+        prev1.next = dummy2.next
+    
+        self.head = dummy1.next
+    
+#   +===================================================+
+#   |               WRITE YOUR CODE HERE                |
+#   | Description:                                      |
+#   | - This method partitions a linked list around a   |
+#   |   value `x`.                                      |
+#   | - It rearranges the nodes so that all nodes less  |
+#   |   than `x` come before all nodes greater or equal |
+#   |   to `x`.                                         |
+#   |                                                   |
+#   | Tips:                                             |
+#   | - We use two dummy nodes, `dummy1` and `dummy2`,  |
+#   |   to build two separate lists: one for elements   |
+#   |   smaller than `x` and one for elements greater   |
+#   |   or equal to `x`.                                |
+#   | - `prev1` and `prev2` help us keep track of the   |
+#   |   last nodes in these lists.                      |
+#   | - Finally, we merge these two lists by setting    |
+#   |   `prev1.next = dummy2.next`.                     |
+#   | - The head of the resulting list becomes          |
+#   |   `dummy1.next`.                                  |
+#   +===================================================+
+
+my_linked_list = LinkedList(1)
+
+my_linked_list.append(4)
+my_linked_list.append(5)
+my_linked_list.append(9)
+my_linked_list.append(7)
+my_linked_list.append(2)
+
+my_linked_list.partition_list(5)
+my_linked_list.print_list()
