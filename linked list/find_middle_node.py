8fbd7ccedafbf67669587ea5e66beb4a3deb71b5
Jerricke <eejerrickw@gmail.com>
2023-12-20 14:38:18 -0800

    leetcode LL find middle node

---
+++ linked list/find_middle_node.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+        
+
+    def find_middle_node(self):
+        fast = self.head
+        slow = self.head
+        counter = 0
+        while fast.next is not None and fast is not None:
+            if counter%2 == 0:
+                slow = slow.next
+            fast = fast.next
+            counter += 1
+        return slow
+
+
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+print( my_linked_list.find_middle_node().value )
--- linked list/linked_list_constructor.py
+++ linked list/linked_list_constructor.py
@@ -95,16 +95,16 @@ class LinkedList:
         return True
         
     def remove(self, index):
-        if index < 0 or index > self.length:
-            return False
+        if index < 0 or index >= self.length:
+            return None
         if index == 0:
             return self.pop_first()
-        if index == self.length:
+        if index == self.length - 1:
             return self.pop()
-        temp = self.get(index-1)
-        removal = temp.next
-        temp.next = removal.next
-        removal.next = None
+        pre = self.get(index - 1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
         self.length -= 1
         return temp
     
