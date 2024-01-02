fc889ff23878971c1c5590a6a7996d09d416df2b
Jerricke <eejerrickw@gmail.com>
2024-01-02 14:32:34 -0800

    dll reverse

---
+++ doubly linked list/dll_reverse.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+        
+
+    def reverse(self):
+        if self.head is None:
+            return False
+        
+        temp = self.head
+        new_head = self.tail
+        new_tail = self.head
+        
+        for _ in range(self.length):
+            before = temp.prev
+            after = temp.next
+            temp.prev = after
+            temp.next = before
+            temp = temp.prev
+        self.head = new_head
+        self.tail = new_tail
+        
+        return True
