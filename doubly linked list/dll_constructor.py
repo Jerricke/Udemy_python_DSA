57082e4c201156703b5542b7855ae6f05aa397c1
Jerricke <eejerrickw@gmail.com>
2024-01-02 13:16:00 -0800

    DLL insert

--- doubly linked list/dll_constructor.py
+++ doubly linked list/dll_constructor.py
@@ -5,7 +5,7 @@ class Node:
         self.prev = None
         
 
-class doublyLinkedList:
+class DoublyLinkedList:
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
@@ -17,7 +17,7 @@ class doublyLinkedList:
         while temp is not None:
             print(temp.value)
             temp = temp.next
-    
+        
     def append(self, value):
         new_node = Node(value)
         if self.head is None:
@@ -26,25 +26,25 @@ class doublyLinkedList:
         else:
             self.tail.next = new_node
             new_node.prev = self.tail
-            self.tal = new_node
+            self.tail = new_node
         self.length += 1
         return True
-    
+
     def pop(self):
-        if self.lenghth == 0:
+        if self.length == 0:
             return None
         temp = self.tail
         if self.length == 1:
             self.head = None
-            self.tail = None
-        else:
+            self.tail = None 
+        else:       
             self.tail = self.tail.prev
             self.tail.next = None
             temp.prev = None
         self.length -= 1
         return temp
-        
-    def prepend(self,value):
+
+    def prepend(self, value):
         new_node = Node(value)
         if self.length == 0:
             self.head = new_node
@@ -55,7 +55,7 @@ class doublyLinkedList:
             self.head = new_node
         self.length += 1
         return True
-    
+
     def pop_first(self):
         if self.length == 0:
             return None
@@ -66,37 +66,49 @@ class doublyLinkedList:
         else:
             self.head = self.head.next
             self.head.prev = None
-            temp.next = None
+            temp.next = None      
         self.length -= 1
         return temp
-        
+
     def get(self, index):
         if index < 0 or index >= self.length:
             return None
         temp = self.head
-        if index < self.lenghth/2:
+        if index < self.length/2:
             for _ in range(index):
                 temp = temp.next
         else:
             temp = self.tail
             for _ in range(self.length - 1, index, -1):
-                temp = temp.prev
+                temp = temp.prev  
         return temp
-    
+        
     def set_value(self, index, value):
         temp = self.get(index)
         if temp:
             temp.value = value
             return True
         return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
 
-my_dll = doublyLinkedList(2)
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
 
-my_dll.append(3)
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+        
+        self.length += 1   
+        return True  
 
-my_dll.prepend(1)
-my_dll.prepend(0)
+  
 
-my_dll.pop_first()
-
-my_dll.print_list()
