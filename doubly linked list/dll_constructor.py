de9816452a38ced0f57af39b7565d514d68a2c81
Jerricke <eejerrickw@gmail.com>
2024-01-02 13:20:16 -0800

    DLL remove

--- doubly linked list/dll_constructor.py
+++ doubly linked list/dll_constructor.py
@@ -110,5 +110,30 @@ class DoublyLinkedList:
         self.length += 1   
         return True  
 
-  
+    def remove(self, index):
+        if index < 0 or index > self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length-1:
+            return self.pop()
 
+        temp = self.get(index)
+
+        temp.next.prev = temp.prev
+        temp.prev.next = temp.next
+
+        temp.prev = None
+        temp.next = None
+
+        self.length -= 1
+        return temp
+    
+dll = DoublyLinkedList(1)
+
+dll.append(2)
+dll.append(3)
+
+dll.remove(1)
+
+dll.print_list()
