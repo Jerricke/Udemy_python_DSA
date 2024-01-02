649dc99b309fb651a05f593be794e4024eeb3347
Jerricke <eejerrickw@gmail.com>
2024-01-02 12:44:55 -0800

    DLL popfirst

--- doubly linked list/dll_constructor.py
+++ doubly linked list/dll_constructor.py
@@ -55,6 +55,20 @@ class doublyLinkedList:
             self.head = new_node
         self.length += 1
         return True
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
         
 
 my_dll = doublyLinkedList(2)
@@ -62,4 +76,8 @@ my_dll = doublyLinkedList(2)
 my_dll.append(3)
 
 my_dll.prepend(1)
+my_dll.prepend(0)
+
+my_dll.pop_first()
+
 my_dll.print_list()
