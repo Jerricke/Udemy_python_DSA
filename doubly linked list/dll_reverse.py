5f88f426772313329ca6e6218a8d1de73a41e984
Jerricke <eejerrickw@gmail.com>
2024-01-03 10:47:46 -0800

    stack and queue constructors

--- doubly linked list/dll_reverse.py
+++ doubly linked list/dll_reverse.py
@@ -49,3 +49,15 @@ class DoublyLinkedList:
         self.tail = new_tail
         
         return True
+    
+    def reverse_solution(self):
+        temp = self.head
+        while temp is not None:
+            # swap the prev and next pointers of node points to
+            temp.prev, temp.next = temp.next, temp.prev
+            
+            # move to the next node
+            temp = temp.prev
+            
+        # swap the head and tail pointers
+        self.head, self.tail = self.tail, self.head
--- doubly linked list/dll_swappairs.py
+++ doubly linked list/dll_swappairs.py
@@ -32,42 +32,52 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
-    def swap_pairs(self):
-        if self.head is None:
-            return False
-        if self.length <2:
-            return False
-        else:
-            pair1 = self.head
-            self.head = pair1.next
-            while pair1 and pair1.next is not None:
-                pair2 = pair1.next
-                pair1.next, pair1.prev, pair2.next, pair2.prev = pair2.next, pair2.prev, pair1.next, pair1
-                print(pair1.value, pair2.value)
-                if pair1.next:
-                    pair1.next.prev = pair1
-                    print(pair1.next.value)
-                pair1 = pair1.next
-                print('end loop')
-
-            return True
-        
     # def swap_pairs(self):
     #     if self.head is None:
     #         return False
-    #     if self.length < 2:
+    #     if self.length <2:
     #         return False
     #     else:
     #         pair1 = self.head
     #         self.head = pair1.next
-    #         while pair1 and pair1.next:
+    #         while pair1 and pair1.next is not None:
     #             pair2 = pair1.next
     #             pair1.next, pair1.prev, pair2.next, pair2.prev = pair2.next, pair2.prev, pair1.next, pair1
+    #             print(pair1.value, pair2.value)
     #             if pair1.next:
     #                 pair1.next.prev = pair1
+    #                 print(pair1.next.value)
     #             pair1 = pair1.next
+    #             print('end loop')
 
     #         return True
+        
+    def swap_pairs(self):
+        dummy_node = Node(0)
+        dummy_node.next = self.head
+        previous_node = dummy_node
+    
+        while self.head and self.head.next:
+            first_node = self.head
+            second_node = self.head.next
+    
+            previous_node.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+    
+            second_node.prev = previous_node
+            first_node.prev = second_node
+    
+            if first_node.next:
+                first_node.next.prev = first_node
+    
+            self.head = first_node.next
+            previous_node = first_node
+    
+        self.head = dummy_node.next
+    
+        if self.head:
+            self.head.prev = None
         
 my_dll = DoublyLinkedList(1)
 my_dll.append(2)
