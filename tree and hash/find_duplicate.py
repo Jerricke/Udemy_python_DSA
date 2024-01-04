5f654ef7909f3fa356c94e71ed6e0bc9c75af3b6
Jerricke <eejerrickw@gmail.com>
2024-01-04 11:22:24 -0800

    find dupe leetcode

---
+++ tree and hash/find_duplicate.py
@@ -0,0 +1,21 @@
+def find_duplicates(li):
+    mapper = {}
+    dupe = []
+    for i in li:
+        if i not in mapper:
+            mapper[i] = True
+        else:
+            dupe.append(i)
+    return dupe
+
+
+
+
+print ( find_duplicates([1, 2, 3, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 3]) )
+print ( find_duplicates([1, 1, 1, 1, 1]) )
+print ( find_duplicates([1, 2, 3, 3, 3, 4, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([1, 1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([]) )
+
