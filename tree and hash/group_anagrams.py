7bc334fe9158c6841bcb90632211fc6ae878ba70
Jerricke <eejerrickw@gmail.com>
2024-01-04 12:00:55 -0800

    group anagrams leetcode

---
+++ tree and hash/group_anagrams.py
@@ -0,0 +1,47 @@
+def group_anagrams(grp):
+    mapper = {}
+    for word in grp:
+        cannonical = ''.join(sorted(word))
+        if cannonical in mapper:
+            mapper[cannonical].append(word)
+        else:
+            mapper[cannonical] = [word]
+    return list(mapper.values())
+
+
+# def group_anagrams(strings):
+#     anagram_groups = {}
+#     for string in strings:
+#         canonical = ''.join(sorted(string))
+#         if canonical in anagram_groups:
+#             anagram_groups[canonical].append(string)
+#         else:
+#             anagram_groups[canonical] = [string]
+#     return list(anagram_groups.values())
+
+
+
+print("1st set:")
+print( group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) )
+
+print("\n2nd set:")
+print( group_anagrams(["abc", "cba", "bac", "foo", "bar"]) )
+
+print("\n3rd set:")
+print( group_anagrams(["listen", "silent", "triangle", "integral", "garden", "ranged"]) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    1st set:
+    [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+
+    2nd set:
+    [['abc', 'cba', 'bac'], ['foo'], ['bar']]
+
+    3rd set:
+    [['listen', 'silent'], ['triangle', 'integral'], ['garden', 'ranged']]
+
+"""
