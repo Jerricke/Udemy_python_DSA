3041d2a542f301ae44bacfa856fe4d56073fe75b
Jerricke <eejerrickw@gmail.com>
2024-01-09 11:53:20 -0800

    sinkdown

--- heaps/heap_constructor.py
+++ heaps/heap_constructor.py
@@ -21,18 +21,50 @@ class MaxHeap:
         while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
             self._swap(current, self._parent(current))
             current = self._parent(current)
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+
+        return max_value
+    
+    def _sink_down(self, index):
+        max_index = index
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            if (left_index < len(self.heap) and self.heap[left_index] > self.heap[max_index]):
+                max_index = left_index
+            
+            if (right_index < len(self.heap) and self.heap[right_index] > self.heap[max_index]):
+                max_index = right_index
+            
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                return
+
 
     
 mh = MaxHeap()
-mh.insert(99)
-mh.insert(72)
-mh.insert(61)
-mh.insert(58)
-
-print(mh.heap)
-
-mh.insert(100)
-print(mh.heap)
-
+mh.insert(95)
 mh.insert(75)
+mh.insert(72)
+mh.insert(55)
+mh.insert(60)
+mh.insert(50)
+mh.insert(65)
+
+print(mh.heap)
+mh.remove()
+print(mh.heap)
+mh.remove()
 print(mh.heap)
