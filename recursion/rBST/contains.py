13bb36222980566c4454ee84d2f32d6d66351167
Jerricke <eejerrickw@gmail.com>
2024-01-09 12:46:10 -0800

    recursive insert

--- recursion/rBST/contains.py
+++ recursion/rBST/contains.py
@@ -38,8 +38,23 @@ class BinarySearchTree:
         if value > current_node.value:
             return self.__r_contains(current_node.right, value)
         
+    def __r_insert(self, current_node, value):
+        if current_node == None:
+            return Node(value)
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left, value)
+        if value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right, value)
+        return current_node 
+
     def r_contains(self, value):
         return self.__r_contains(self.root, value)
+    
+    def r_insert(self, value):
+        if self.root == None:
+            self.root = Node(value)
+        self.__r_insert(self.root, value)
+
 
 mt = BinarySearchTree()
 
