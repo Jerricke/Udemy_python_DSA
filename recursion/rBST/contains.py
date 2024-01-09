a9a3de583e0cf63541bcfd15e8d1fbba761d8a6b
Jerricke <eejerrickw@gmail.com>
2024-01-09 13:08:03 -0800

    recursive delete node for bst

--- recursion/rBST/contains.py
+++ recursion/rBST/contains.py
@@ -39,7 +39,7 @@ class BinarySearchTree:
             return self.__r_contains(current_node.right, value)
         
     def __r_insert(self, current_node, value):
-        if current_node == None:
+        if current_node == None: 
             return Node(value)
         if value < current_node.value:
             current_node.left = self.__r_insert(current_node.left, value)
@@ -54,6 +54,34 @@ class BinarySearchTree:
         if self.root == None:
             self.root = Node(value)
         self.__r_insert(self.root, value)
+    
+    def min_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+
+    def __delete_node(self, current_node, value):
+        if current_node == None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left == None and current_node.right == None:
+                return None
+            elif current_node.left == None:
+                current_node = current_node.right
+            elif current_node.right == None:
+                current_node = current_node.left
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+        return current_node
+    
+    def delete_node(self, value):
+        self.__delete_node(self.root, value)
 
 
 mt = BinarySearchTree()
